a57dc07ac4edcc92d45d4ddef370d7bb98165bec
nafilfiles <nickhopsy5@comcast.net>
2026-02-09 22:22:44 -0500

    Initialuzed UV, created basic rename function in CLI

---
+++ main.py
@@ -0,0 +1,19 @@
+import rename
+import pathvalidate as pv
+
+def main():
+    print("Hello from church-automation-python!")
+    old_name = input("Enter the file you want to rename: \n")
+    while old_name != pv.sanitize_filename(old_name, platform="auto") or old_name == "":
+        old_name = input("Filename contains forbidden characters\n")
+
+    new_name = input("Enter the new name for the file: \n")
+    while new_name != pv.sanitize_filename(new_name, platform="auto") or new_name == "":
+        new_name = input("Filename contains forbidden characterswhat\n")
+    # print(old_name)
+    # print(new_name)
+    rename.renamefile(old_name, new_name)
+
+
+if __name__ == "__main__":
+    main()
---
+++ rename.py
@@ -0,0 +1,30 @@
+import os
+import pathvalidate as pv
+
+
+# function to rename files, taking in the file path of old name and new name
+def renamefile(oldname, newname):
+    try:
+        os.rename(oldname, newname)
+    except FileNotFoundError:
+        print(f"File '{oldname}' does not exist")
+    except FileExistsError:
+        print(f"Error: The file '{newname}' already exists.")
+    except PermissionError:
+        print("Error: Permission denied. Check file permissions.")
+    except OSError:
+        print("Bad characters")
+    except OSError as e:
+        print(f"An unexpected OS error occurred: {e}")
+    except Exception as e:
+         print(f"{e}")
+        
+def validation(string):
+    #  print(pv.sanitize_filename(string))
+    if string == pv.sanitize_filename(string):
+         print("True")
+    else:
+         print("False")
+
+# input = input("enter a string")
+# validation(input)
