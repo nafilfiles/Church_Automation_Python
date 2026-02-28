a63f82f2502732432cca6bae6ca64b8d68ef042e
nafilfiles <nickhopsy5@comcast.net>
2026-02-27 21:38:23 -0500

    created filepath test, used pathvalidate's function natively

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from src import rename
+import pathvalidate as pv
 
 def main():
+    old_name = ""
     print("Hello from church-automation-python!")
-    old_name = input("Enter the file you want to rename: \n")
-    while not rename.validation(old_name):
-        old_name = input("Filename contains forbidden characters\n")
+    # old_name = input("Enter the file you want to rename: \n")
+    while not pv.is_valid_filename(old_name):
+        print("Filename contains forbidden characters")
+        old_name = input("Enter the file you want to rename:\n")
 
     new_name = input("Enter the new name for the file: \n")
-    while not rename.validation(new_name):
-        new_name = input("Filename contains forbidden characterswhat\n")
+    while not pv.is_valid_filename(new_name):
+        new_name = input("Filename contains forbidden characters\n")
     # print(old_name)
     # print(new_name)
     rename.renamefile(old_name, new_name)
--- tests/rename_test.py
+++ tests/rename_test.py
@@ -1,5 +1,41 @@
-from src import rename
+import pathvalidate as pv
+import platform
+
 
 def test_validation():
-    assert rename.validation("validname.txt")
-    assert not rename.validation("invalid<name>.txt")
+    os = platform.system()
+    if os == "Windows":
+        # Should return true as valid filenames
+        assert pv.is_valid_filename("validname.txt")
+        assert pv.is_valid_filename("123456790")
+        assert pv.is_valid_filename("qwertyuiopasdfghjklzxcvbnm")
+        assert pv.is_valid_filename("!@#$%^&()")
+        assert pv.is_valid_filename(r"-_=+[{}]'';.,~`")
+
+        # Should return false as invalid filenames
+        assert not pv.is_valid_filename("invalid<name>.txt")
+        assert not pv.is_valid_filename("invalid:name")
+        assert not pv.is_valid_filename('invalid"name".txt')
+        assert not pv.is_valid_filename("invalid/name")
+        assert not pv.is_valid_filename(r"invalid\ name.txt")
+        assert not pv.is_valid_filename("invalid|name.txt")
+        assert not pv.is_valid_filename("invalid?name.txt")
+        assert not pv.is_valid_filename("invalid*name.txt")
+
+    if os == "Linux":
+        assert pv.is_valid_filename("validname.txt")
+        assert pv.is_valid_filename("123456790")
+        assert pv.is_valid_filename("qwertyuiopasdfghjklzxcvbnm")
+        assert pv.is_valid_filename("!@#$%^&*()")
+        assert pv.is_valid_filename(r"-_=+{}[]\|'';:.>,<?`~")
+        assert not pv.is_valid_filename("invalid/name.txt")
+
+    if os == "Darwin": # MacOs
+        assert pv.is_valid_filename("validname.txt")
+        assert pv.is_valid_filename("123456790")
+        assert pv.is_valid_filename("qwertyuiopasdfghjklzxcvbnm")
+        assert pv.is_valid_filename("!@#$%^&*()")
+        assert pv.is_valid_filename(r"-_=+{}[]\|'';/.>,<?`~")
+        assert not pv.is_valid_filename("invalid:name.txt")
+
+    
